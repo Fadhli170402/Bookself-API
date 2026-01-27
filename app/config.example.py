023365acb4c4b507c4d49aa2dbc4fd98e5d2aaac
Fadhli170402 <fadliadliana5@gmail.com>
2026-01-27 16:05:50 +0700

    Remove Config

--- app/config.example.py
+++ app/config.example.py
@@ -1,11 +1,11 @@
-SECRET_KEY = 'abcd1234'
+SECRET_KEY = 'your secret key here'
 JWT_EXPIRED_SECONDS = 3600
 
-DB_USER = "root"
+DB_USER = "your_db_user"
 DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_NAME = "bookself"
+DB_HOST = "your_db_host"
+DB_NAME = "your_db_name"
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-JWT_SECRET_KEY = 'supersecretkey'
+JWT_SECRET_KEY = 'your_jwt_secret_key_here'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
